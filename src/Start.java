babe00bc77f10fb6f55ef8a47cd0106ef9edde35
Nicolo' Ferri <irr3f92@gmail.com>
2023-06-05 15:35:44 +0200

    Primo commento

---
+++ src/Start.java
@@ -0,0 +1,6 @@
+public class Start {
+    public static void main ( String[] args) {
+        System.out.println("Hello develhope java student");
+        System.out.println("21/11/1992");
+    }
+}
